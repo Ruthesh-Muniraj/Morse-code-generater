a1c3ff6c1ccdea2fe3539f81309aa94ff260335e
Ruthesh Muniraj <47059855+Ruthesh-Muniraj@users.noreply.github.com>
2020-04-02 04:13:38 +0000

    Update Morse.cpp

--- Morse.cpp
+++ Morse.cpp
@@ -1,13 +1,12 @@
 #include<iostream>
 #include<string> 
 #include<algorithm>
-using namespace std;
 
 int main(){
     
-    string text;
-    string alphabet="abcdefghijklmnopqrstuvwxyz1234567890.,?/@!& ";
-    string Morse[]= {".-","-...","-.-.","-..",".","..-.","--.",
+    std::string text;
+    std::string alphabet="abcdefghijklmnopqrstuvwxyz1234567890.,?/@!& ";
+    std::string Morse[]= {".-","-...","-.-.","-..",".","..-.","--.",
                     "....","..",".---","-.-",".-..","--","-.",
                     "---",".--.","--.-",".-.","...","-","..-",
                     "...-",".--","-..-","-.--","--..",".----",
@@ -15,22 +14,22 @@ int main(){
                     "--...","---..","----.","-----",".-.-.-",
                     "--..--","..--..","-..-.",".--.-.","-.-.--",".-..."," / "};
     
-    cout<<"+-----------------------------------------+\n";
-    cout<<">In morse an character can represented by \n>  ~Dot(.) and Dash(-) \n>  ~(/) Represent space between words   >_\n";
-    cout<<"+-----------------------------------------+\n\n";
+    std::cout<<"+-----------------------------------------+\n";
+    std::cout<<">In morse an character can represented by \n>  ~Dot(.) and Dash(-) \n>  ~(/) Represent space between words   >_\n";
+    std::cout<<"+-----------------------------------------+\n\n";
     
-    cout<<"Enter the text to conver in to string : ";
-    getline(cin,text);
+    std::cout<<"Enter the text to conver in to string : ";
+    getline(std::cin,text);
     transform(text.begin(),text.end(),text.begin(),::tolower);
  
     if(text == "sos"){
-    cout<<"...---...";
+    std::cout<<"...---...";
     }
  
     else{
       for(const char& x : text){
           int m = alphabet.find(x,0);
-          cout<<Morse[m]<<"   ";
+          std::cout<<Morse[m]<<"   ";
       }
     }
 }
